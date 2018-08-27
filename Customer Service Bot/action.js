ca71450cb3fbbf8354e1070d729c178a2fdc3d18
priyaVora <prvora89@gmail.com>
2018-08-26 21:45:03 -0600

    Update: user can send response and bot can send response back

--- Customer Service Bot/action.js
+++ Customer Service Bot/action.js
@@ -11,32 +11,54 @@ function send_message() {
   document.getElementById("demo").innerHTML = client_message;
   if (client_message != null && client_message != "") {
   console.log("Client Side Message: " + client_message);
+
+  var innerDiv = document.createElement('div');
+  innerDiv.className = 'chat self';
+  innerDiv.id = "ans-show";
+    document.getElementById("chatlogs").appendChild(innerDiv);
+
+  var userPhotoDiv = document.createElement('div');
+  userPhotoDiv.className = 'user-photo';
+  innerDiv.appendChild(userPhotoDiv);
+
+  var node = document.createElement("p");
+  node.className = "chat-message";
+  var responseNode = document.createTextNode(client_message);
+  node.appendChild(responseNode);
+
+  innerDiv.appendChild(node);
+document.getElementById("chatlogs").appendChild(innerDiv);
+
   chat_bot_response();
     }
 }
 
+$(document).ready(function(){
+  $('#chatlogs').animate({
+  scrollTop: $('#chatlogs').get(0).scrollHeight}, 2000);
+});
+
 function chat_bot_response() {
   var chat_reponse = questions[0];
-  var iDiv = document.createElement('div');
-iDiv.id = 'block';
-iDiv.className = 'block';
-document.getElementsByTagName('body')[0].appendChild(iDiv);
 
-var innerDiv = document.createElement('div');
-innerDiv.className = 'chat friend';
-innerDiv.id = "result";
+  var innerDiv = document.createElement('div');
+  innerDiv.className = 'chat friend';
+  innerDiv.id = "result";
+    document.getElementById("chatlogs").appendChild(innerDiv);
 
-var userPhotoDiv = document.createElement('div');
-userPhotoDiv.className = 'user-photo';
-innerDiv.appendChild(userPhotoDiv);
+  var userPhotoDiv = document.createElement('div');
+  userPhotoDiv.className = 'user-photo';
+  innerDiv.appendChild(userPhotoDiv);
 
-var node = document.createElement("p");
-var responseNode = document.createTextNode(questions[0]);
-node.appendChild(responseNode);
+  var node = document.createElement("p");
+  node.className = "chat-message";
+  var responseNode = document.createTextNode(questions[0]);
+  node.appendChild(responseNode);
 
-innerDiv.appendChild(node);
-
-iDiv.appendChild(innerDiv);
-document.getElementsByTagName('body')[0].appendChild(iDiv);
+  innerDiv.appendChild(node);
+  var objDiv = document.getElementById("chatlogs");
+  objDiv.scrollTop = objDiv.scrollHeight;
+document.getElementById("chatlogs").appendChild(innerDiv);
+$('#chatlogs').scrollTop($('#chatlogs')[0].scrollHeight);
 console.log("NATASHA: " + questions[0]);
 }
