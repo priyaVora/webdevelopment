8f9e9f3d00aa09933fe2f4f96a75c5f2bb29a676
priyaVora <prvora89@gmail.com>
2018-09-17 18:35:00 -0700

    Update

--- smart-brain/server.js
+++ smart-brain/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -23,6 +23,13 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  login: [
+    {
+      id: '987',
+      has: '',
+      email:'john@gmail.com'
+    }
   ]
 }
 
@@ -31,6 +38,14 @@ app.get('/', (req, res) =>  {
 })
 
 app.post('/signin', (req, res) => {
+  // // Load hash from your password DB.
+  // bcrypt.compare("bacon", hash, function(err, res) {
+  //     // res == true
+  // });
+  // bcrypt.compare("veggies", hash, function(err, res) {
+  //     // res = false
+  // });
+
   if(req.body.email === database.users[0].email &&
   req.body.password == database.users[0].password) {
     res.json('success');
@@ -41,6 +56,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const {email, name, password } = req.body;
+  bcrypt.hash("password", null, null, function(err, hash) {
+    console.log(hash);
+  });
   database.users.push({
     id: '125',
     name: name,
@@ -80,6 +98,10 @@ app.get('/profile/:id', (req,res) => {
     res.status(400).json('not found');
   }
 })
+
+
+
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
