d201d9edd1ea9920582d56ee74b25a2395c1af10
priyaVora <prvora89@gmail.com>
2018-10-20 15:58:22 -0600

    Setup Project with an Floorplan Example

---
+++ floorplan/floorplan/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import DropArea from "./components/DropArea/DropArea";
+
+class index extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DropArea/>
+      </div>
+    );
+  }
+}
+
+export default index;
