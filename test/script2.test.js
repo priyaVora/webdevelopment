7e4e20e18277ac00dc9bb877c954dfeb5cc97b36
priyaVora <prvora89@gmail.com>
2018-11-02 00:52:22 -0600

    Jest Testing Finished

--- test/script2.test.js
+++ test/script2.test.js
@@ -19,3 +19,22 @@ it("calls swapi api to get people with a promise", ()=> {
     expect(data.results.length).toBeGreaterThan(5)
   })
 })
+
+
+it("get people returns count and results", ()=> {
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+          count: 89,
+          results: [0,1,2,3,4,5]
+      })
+  }))
+
+expect.assertions(4)
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+    expect(data.count).toEqual(89)
+    expect(data.results.length).toBeGreaterThan(5)
+  })
+})
