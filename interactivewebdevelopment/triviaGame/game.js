1709f9ba64f74d0b3d9f0a707ca8ea898590087f
Priya <prvora89@gmail.com>
2019-07-29 00:30:07 -0600

    Update

--- interactivewebdevelopment/triviaGame/game.js
+++ interactivewebdevelopment/triviaGame/game.js
@@ -1,21 +1,104 @@
 class Game { 
     constructor() {
+    this.current = undefined;    
     document.getElementById('people').addEventListener('click',this.people);
     document.getElementById('starships').addEventListener('click',this.starships);
     document.getElementById('planets').addEventListener('click',this.planets);
     document.getElementById('vehicles').addEventListener('click',this.vehicles);
+    document.getElementById('films').addEventListener('click',this.films);
+    document.getElementById('species').addEventListener('click',this.species);
 }
 
     makeApiCall = (self, url, max) => { 
         const proxy = 'https://cors-anywhere.herokuapp.com/';
         let randomIndex = Math.floor(Math.random() * Math.floor(max));
+        this.current = randomIndex;
         url = proxy + url + randomIndex;
         return fetch(url).then(res=>{
             return { 
                 status : res.status, 
-                data: res.json()
+                data: res.json(),
+                current: randomIndex
             } 
         });
+    }
+
+    game = (questions, counter) => { 
+        console.log(questions);
+        this.next = () => { 
+            counter++;
+            if(questions[counter] !== undefined) { 
+                this.game(questions,counter);
+            } else { 
+                var node = document.getElementById('game');  
+                while (node.firstChild) {
+                    node.removeChild(node.firstChild);
+                }             
+                var textnode = document.createTextNode("Sweet you are done!");
+                node.appendChild(textnode); 
+            }
+        }
+         let question = questions[counter];
+            var node = document.getElementById('game');  
+                while (node.firstChild) {
+                    node.removeChild(node.firstChild);
+                }             
+                var textnode = document.createTextNode("Q: " + question);
+                node.appendChild(textnode);
+
+                
+                var button = document.createElement("button"); 
+                var buttonNode = document.createTextNode('Next');
+                button.addEventListener('click', this.next);
+                // document.getElementById('next').addEventListener('click',this.next);
+                button.appendChild(buttonNode);
+                node.appendChild(button); 
+
+
+    
+    }
+
+    question = (type, data) =>{ 
+        switch(type) { 
+            case 'people': 
+            let q = [];
+                var node = document.getElementById('game');  
+                while (node.firstChild) {
+                    node.removeChild(node.firstChild);
+                }             
+                var textnode = document.createTextNode("Q:");
+                node.appendChild(textnode);
+                console.log(this.current);
+
+
+                var keys= [];
+                
+                for(var k in data) { 
+                    keys.push(k);
+                    if(typeof data[k] === 'object') { 
+                        q.push('How many ' + data['name'] + ' does ' + " have?");
+                    } else { 
+                        q.push("What is" + data['name'] + "\'s " + k + "?")
+                    }
+                } 
+
+                this.game(q,0);
+               
+
+                break;
+            case 'starships': 
+                break; 
+            case 'planets': 
+                break; 
+            case 'vehicles': 
+                break;  
+            case 'films': 
+                break;
+            case 'species': 
+                break;
+            default: 
+            //
+        }
     }
 
     people = () =>{
@@ -26,10 +109,12 @@ class Game {
                 if(person.status !== 200) { 
                     throw new Error(404);
                 } else { 
+                    self.current = person.current;
                     return person.data;
                 }
             }).then(result => {
                 console.log(result);
+                self.question('people', result);
             })
             .catch(err => {   
             self.people();
@@ -92,11 +177,39 @@ class Game {
     }   
     
     films = () => { 
-        console.log('Films');
+        document.getElementById('title').innerHTML = "Star Wars Trivia Game - Films";
+        let self = this;
+        self.makeApiCall(self, 'https://swapi.co/api/films/', 7)
+        .then(planets => {
+            if(planets.status !== 200) { 
+                throw new Error(404);
+            } else { 
+                return planets.data;
+            }
+        }).then(result => {
+            console.log(result);
+        })
+        .catch(err => { 
+            self.films();
+        });
     }
     
     species =() =>  { 
-        console.log('Species');
+        document.getElementById('title').innerHTML = "Star Wars Trivia Game - Species";
+        let self = this;
+        self.makeApiCall(self, 'https://swapi.co/api/species/', 37)
+        .then(planets => {
+            if(planets.status !== 200) { 
+                throw new Error(404);
+            } else { 
+                return planets.data;
+            }
+        }).then(result => {
+            console.log(result);
+        })
+        .catch(err => { 
+            self.films();
+        });
     }
     
     randomCategory() { 
